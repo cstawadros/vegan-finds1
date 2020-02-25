cc8ac7e5a889cdbdf2c6b9cb738d7d1b0233fdf7
cstawadros <61443147+cstawadros@users.noreply.github.com>
2020-02-25 16:16:13 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+
+function on() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
